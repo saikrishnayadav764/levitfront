d65d1c28685457d44e1037f237921a6e0c04746d
saikrishnayadav764 <106056182+saikrishnayadav764@users.noreply.github.com>
2023-10-23 16:19:30 +0530

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Cookies from 'js-cookie';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Navigate} from 'react-router-dom';
 
 const Login = () => {
   const authToken: string | undefined = Cookies.get('authToken');
